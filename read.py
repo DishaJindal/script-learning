d6a0d3326411cdca7be1c985675803698898757b
Sandeep Dcunha <sdcunha@seas.upenn.edu>
2019-04-17 20:53:58 -0400

    Added dataset code

---
+++ read.py
@@ -0,0 +1,10 @@
+import pickle
+import itertools
+
+def read_data_iterator(pickle_file_path):
+    with open(pickle_file_path, 'rb') as f:
+        for i in itertools.count():
+            try:
+                yield pickle.load(f)
+            except EOFError:
+                raise StopIteration
