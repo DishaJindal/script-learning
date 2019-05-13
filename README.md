a0b56ec03d5f66da454e2d06e79ca5e6a91dd2e9
Disha Jindal <dishajindal@Dishas-MacBook-Pro.local>
2019-05-13 15:54:20 -0400

    Prediction cleanup and README

--- README.md
+++ README.md
@@ -1,4 +1,78 @@
 ## Narritive Cloze Event Chain Learning with Bert and Enhanced Features
 
 
-Run bertstory.py to train/test. 
+##Train
+python bertstory.py
+
+####Arguments
+--data: Path of the pickle data file
+
+--output_dir: Output directory for the model files
+
+--device: GPU Id
+
+--sentence: Flag to consider event sentences
+
+--no_context: Flag to ignore event chain and classify only on the basis of candidates
+
+--neeg_dataset: Flag to consider NEEG data format
+
+--story_cloze: Flag to consider Story Cloze data format
+
+--conceptnet: Flag to consider concept net embeddings in input
+
+--candidates: Number of candidates, 5 for MCNC and 2 for Story Cloze Task
+
+--input_size: Number of entries to consider from input dataset
+
+####Examples
+MCNC Sentence Dataset
+
+python bertstory.py --data "dataset/gw_extractions_no_rep_no_fin.pickle" --sentence --output_dir output_sentence --device 2 &>> sentence_logs &
+
+NEEG Dataset
+
+python bertstory.py --data "dataset/neeg.pickle" --neeg_dataset --output_dir output_neeg --device 2 &>neeg_logs&
+
+STORY Cloze Task
+
+python bertstory.py --candidates 2 --data "dataset/story_data.pkl" --output_dir output_story --story_cloze --sentence --device 0 --input_size 1800 &>>story_logs&
+
+
+##Predict
+
+python predict.py
+
+####Arguments
+
+--data: Path of the pickle data file
+
+--model_dir: Directory containing model files
+
+--device: GPU Id
+
+--candidates: Number of candidates, 5 for MCNC and 2 for Story Cloze Task
+
+--sentence: Flag to consider event sentences
+
+--no_context: Flag to ignore event chain and classify only on the basis of candidates
+
+--neeg_dataset: Flag to consider NEEG data format
+
+--story_cloze: Flag to consider Story Cloze data format
+
+--conceptnet: Flag to consider concept net embeddings in input
+
+
+####Examples
+
+MCNC Sentence Dataset
+
+NEEG Dataset
+
+python predict.py --data "dataset/neeg.pickle" --neeg_dataset --model_dir output_neeg/ --device 2
+
+Story Cloze Dataset
+
+python predict.py --data "dataset/story_winter_val.pkl" --sentence --candidates 2 --model_dir output_story/ --story_cloze --device 2
+
--- predict.py
+++ predict.py
@@ -4,42 +4,23 @@ import os
 parser = argparse.ArgumentParser(description='ScriptLearning')
 parser.add_argument('--data', type=str, default="dataset/gw_extractions_no_rep_no_fin.pickle")  # 1
 parser.add_argument('--device', type=str, default="1")
+parser.add_argument('--candidates', type=int, default=5)  # Narrative Cloze Task has 5 options
+parser.add_argument('--model_dir', type=str)
 
 parser.add_argument('--sentence', default=False, action='store_true')
 parser.add_argument('--no_context', default=False, action='store_true')
 parser.add_argument('--neeg_dataset', default=False, action='store_true')
 parser.add_argument('--story_cloze', default=False, action='store_true')
 parser.add_argument('--conceptnet', default=False, action='store_true')
-
-parser.add_argument('--candidates', type=int, default=5)  # Narrative Cloze Task has 5 options
-parser.add_argument('--model_dir', type=str)
 args = parser.parse_args()
 
 os.environ['TFHUB_CACHE_DIR'] = '.'
 os.environ["CUDA_VISIBLE_DEVICES"] = args.device
 
-import read
-import prepare_data
-import input_builder
 import importlib
-import numpy as np
 import model
-import tensorflow as tf
-
-from sklearn.model_selection import train_test_split
-import pandas as pd
-import tensorflow_hub as hub
-from datetime import datetime
-from pdb import set_trace
-import bert
-from bert import optimization
-from bert import tokenization
-from tensorflow import keras
-import re
 from model import *
-import sys
 import prepare_data as prepare_data
-from prepare_data import tokenize_if_small_enough
 from read import *
 
 tf.logging.set_verbosity(tf.logging.INFO)
@@ -50,15 +31,14 @@ MAX_SEQ_LENGTH = 128
 
 importlib.reload(input_builder)
 
-model_dir = 'output_sentence'
 run_config = tf.estimator.RunConfig(
-    model_dir=model_dir,
+    model_dir=args.model_dir,
     save_summary_steps=0,
     save_checkpoints_steps=0,
     log_step_count_steps=100)
 
 model_fn = model.model_fn_builder(
-    num_labels=5,
+    num_labels=args.candidates,
     learning_rate=LEARNING_RATE,
     num_train_steps=1,
     num_warmup_steps=1)
@@ -68,18 +48,30 @@ estimator = tf.estimator.Estimator(
     config=run_config,
     params={"batch_size": BATCH_SIZE})
 
-dataset = list(read.read_data_iterator(args.data))
-val_data = dataset[int(0.8 * len(dataset)):]
-print("Prediction Set Size: {}", len(val_data))
 
-check_dataset = []
-for ec_dict in val_data:
-    check_dataset.append(ec_dict)
+if args.story_cloze:
+    dataset = list(story_read_data_iterator(args.data))
+else:
+    dataset = list(read_data_iterator(args.data))
+val_dataset = dataset[int(0.9 * len(dataset)):]
+check_dataset = val_dataset[:min(2000, len(val_dataset))]
+print("Prediction Set Size: {}", len(check_dataset))
 
 predict_set = list(
     prepare_data.tokenize_if_small_enough(check_dataset, sentences=args.sentence, no_context=args.no_context, is_neeg=args.neeg_dataset,
                                          conceptnet=args.conceptnet,
-                                          input_size=min(2000, len(val_data))))
+                                          input_size=len(check_dataset)))
+
+bad = []
+for i, c in enumerate(check_dataset):
+    r = list(prepare_data.tokenize_if_small_enough([c], sentences=args.sentence, no_context=args.no_context,
+                                                   is_neeg=args.neeg_dataset,
+                                                   conceptnet=args.conceptnet,
+                                                   input_size=1))
+    if not r:
+        bad.append(i)
+good_gt = [c for i, c in enumerate(check_dataset) if i not in bad]
+
 predict_input_fn = input_builder.input_fn_builder(
     features=predict_set,
     seq_length=MAX_SEQ_LENGTH,
@@ -89,18 +81,9 @@ predict_input_fn = input_builder.input_fn_builder(
 
 predictions = estimator.predict(input_fn=predict_input_fn, yield_single_examples=False)
 predictions_list = list(predictions)
-bad = []
-for i, c in enumerate(check_dataset):
-    r = list(prepare_data.tokenize_if_small_enough([c], sentences=args.sentence, no_context=args.no_context, is_neeg=args.neeg_dataset,
-                                         conceptnet=args.conceptnet,
-                                          input_size=min(2000, len(check_dataset))))
-    if not r:
-        bad.append(i)
+print("Number of predictions {}", len(predictions_list))
 
-good_gt = [c for i, c in enumerate(check_dataset) if i not in bad]
 pred_df = pd.DataFrame.from_dict({'predictions': predictions_list, 'dataset': good_gt})
-print(len(good_gt))
-print(len(predictions_list))
 pred_df['pred_label'] = pred_df.predictions.apply(lambda x: x['labels'])
 pred_df['gt_label'] = pred_df.dataset.apply(lambda x: x['correct'])
 pred_df['correct_pred'] = pred_df.apply(lambda s: s.pred_label == s.gt_label, axis=1)
